6e9cf6d77c5553a10f472e9c9f7cf9134574fcb2
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2021-12-09 16:33:17 +0000

    Schedule degails

--- App.tsx
+++ App.tsx
@@ -15,6 +15,7 @@ import { ThemeProvider } from "styled-components/native";
 import theme from "./src/styles/theme";
 import { CardDetails } from "./src/screens/CardDetails";
 import { Scheduling } from "./src/screens/Scheduling";
+import { ScheduleDetails } from "./src/screens/ScheduleDetails";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -31,7 +32,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Scheduling />
+      <ScheduleDetails />
     </ThemeProvider>
   );
 }
---
+++ src/screens/ScheduleDetails/index.tsx
@@ -0,0 +1,129 @@
+import React from "react";
+import { Accessory } from "../../components/Accesssory";
+import { BackButton } from "../../components/BackButton";
+import { ImageSlider } from "../../components/ImgeSlider";
+
+import speedSVG from "../../assets/speed.svg";
+import accelerationSVG from "../../assets/acceleration.svg";
+import forceSVG from "../../assets/force.svg";
+import gasolineSVG from "../../assets/gasoline.svg";
+import exchangeSVG from "../../assets/exchange.svg";
+import peopleSVG from "../../assets/people.svg";
+
+import { Feather } from "@expo/vector-icons";
+
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  Accessories,
+  Footer,
+  RentalPeriod,
+  CalendarIcon,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  RentalPrice,
+  RentalPriceLabel,
+  RentalPriceDetails,
+  RentalPriceQuota,
+  RentalPriceTotal
+} from "./styles";
+import { Button } from "../../components/Button";
+import { RFValue } from "react-native-responsive-fontsize";
+import { useTheme } from "styled-components";
+
+export const ScheduleDetails = () => {
+  const theme = useTheme();
+
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={() => {}} />
+      </Header>
+
+      <CarImages>
+        <ImageSlider
+          imagesUrl={[
+            "https://e7.pngegg.com/pngimages/215/409/png-clipart-audi-audi.png"
+          ]}
+        />
+      </CarImages>
+
+      <Content
+        contentContainerStyle={{
+          padding: 24,
+          alignItems: "center"
+        }}
+        showsVerticalScrollIndicator={false}
+      >
+        <Details>
+          <Description>
+            <Brand>Ferrari</Brand>
+            <Name>Ferrari</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580,00</Price>
+          </Rent>
+        </Details>
+
+        <Accessories>
+          <Accessory name="380km/km" icon={speedSVG} />
+          <Accessory name="3.2s" icon={accelerationSVG} />
+          <Accessory name="800hp" icon={forceSVG} />
+          <Accessory name="Gasolina " icon={gasolineSVG} />
+          <Accessory name="Auto" icon={exchangeSVG} />
+          <Accessory name="2 Ppssoas" icon={peopleSVG} />
+        </Accessories>
+
+        <RentalPeriod>
+          <CalendarIcon>
+            <Feather
+              name="calendar"
+              size={RFValue(24)}
+              color={theme.colors.shape}
+            />
+          </CalendarIcon>
+
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue>18/06/2021</DateValue>
+          </DateInfo>
+
+          <Feather
+            name="chevron-right"
+            size={RFValue(10)}
+            color={theme.colors.text}
+          />
+
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue>18/06/2021</DateValue>
+          </DateInfo>
+        </RentalPeriod>
+
+        <RentalPrice>
+          <RentalPriceLabel>TOTAL</RentalPriceLabel>
+          <RentalPriceDetails>
+            <RentalPriceQuota>R$ 580 x3 diárias</RentalPriceQuota>
+            <RentalPriceTotal>R$ 2.900</RentalPriceTotal>
+          </RentalPriceDetails>
+        </RentalPrice>
+      </Content>
+
+      <Footer>
+        <Button title="confirmar" />
+      </Footer>
+    </Container>
+  );
+};
